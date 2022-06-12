aa0318ddeabc4a1346d1fdb511c4d27f1726c13c
Richard Hu <r.hu@berkeley.edu>
2022-06-11 18:03:30 -0700

    Add main API

--- models/main.py
+++ models/main.py
@@ -18,47 +18,53 @@ from baseline_constants import MAIN_PARAMS, MODEL_SETTINGS
 from client_manager import ClientManager
 from server import Server
 
-from utils.args import parse_args
 from utils.model_utils import read_data
 
 STAT_METRICS_PATH = "metrics/stat_metrics.csv"
 SYS_METRICS_PATH = "metrics/sys_metrics.csv"
 
+DATASETS = ['sent140', 'femnist', 'shakespeare', 'celeba', 'synthetic', 'reddit']
+
 SECTION_STR = "\n############################## {} ##############################"
 
 logger = logging.getLogger("MAIN")
 
-def main():
+def main(
+    dataset: str,
+    model: str,
+    num_rounds: int,
+    eval_every: int,
+    clients_per_round: int,
+    client_lr: float,
+    batch_size: int = 10,
+    seed: int = 0,
+    metrics_name: str = "metrics",
+    metrics_dir: str = "metrics",
+    use_val_set: bool = False,
+    num_epochs: int = 1
+) -> None:
     start_time = datetime.now()
     ray.init()
 
-    args = parse_args()
-
     # Set the random seed if provided (affects client sampling, and batching)
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed(args.seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
 
-    model_path = f"{args.dataset}/{args.model}.py"
+    model_path = f"{dataset}/{model}.py"
     if not os.path.exists(model_path):
         print("Please specify a valid dataset and a valid model.")
-    model_path = f"{args.dataset}.{args.model}"
+    model_path = f"{dataset}.{model}"
     
     print(SECTION_STR.format(model_path))
     mod = importlib.import_module(model_path)
     ClientModel = getattr(mod, "ClientModel")
 
-    tup = MAIN_PARAMS[args.dataset][args.t]
-    num_rounds = args.num_rounds if args.num_rounds != -1 else tup[0]
-    eval_every = args.eval_every if args.eval_every != -1 else tup[1]
-    clients_per_round = args.clients_per_round if args.clients_per_round != -1 else tup[2]
-
     # Create 2 models
     model_settings = MODEL_SETTINGS[model_path]
-    if args.lr != -1:
-        model_settings[0] = args.lr
-    model_settings.insert(0, args.seed)
+    model_settings[0] = client_lr
+    model_settings.insert(0, seed)
     model_settings = tuple(model_settings)
 
     # Get cpu or gpu device for training.
@@ -66,7 +72,7 @@ def main():
 
     num_client_managers = torch.cuda.device_count()
     print(f"Spawning {num_client_managers} Client Managers using {device} device")
-    client_managers = setup_client_managers(num_client_managers, seed=args.seed, device=device)
+    client_managers = setup_client_managers(num_client_managers, seed=seed, device=device)
 
     # Create client model, and share params with server model
     client_model = ClientModel(*model_settings)
@@ -77,19 +83,19 @@ def main():
     # Create clients
     clients = setup_clients(
         client_managers=client_managers,
-        dataset=args.dataset,
+        dataset=dataset,
         model=ClientModel,
         model_settings=model_settings,
-        use_val_set=args.use_val_set
+        use_val_set=use_val_set
     )
     client_ids, client_groups, client_num_samples = server.get_clients_info()
     print(f"Clients in Total: {len(clients)}")
 
     # Initial status
     print("--- Random Initialization ---")
-    stat_writer_fn = get_stat_writer_function(client_ids, client_groups, client_num_samples, args)
-    sys_writer_fn = get_sys_writer_function(args)
-    print_stats(0, server, clients, client_num_samples, args, stat_writer_fn, args.use_val_set)
+    stat_writer_fn = get_stat_writer_function(client_ids, client_groups, client_num_samples, metrics_name, metrics_dir)
+    sys_writer_fn = get_sys_writer_function(metrics_name, metrics_dir)
+    print_stats(0, server, client_num_samples, stat_writer_fn, use_val_set)
 
     # Simulate training
     for i in range(num_rounds):
@@ -99,24 +105,24 @@ def main():
         server.select_clients(i, online(clients), num_clients=clients_per_round)
 
         # Simulate server model training on selected clients" data
-        server.train_clients(num_epochs=args.num_epochs, batch_size=args.batch_size)
+        server.train_clients(num_epochs=num_epochs, batch_size=batch_size)
         
         # Update server model
         server._update_model()
 
         # Test model
         if (i + 1) % eval_every == 0 or (i + 1) == num_rounds:
-            print_stats(i + 1, server, clients, client_num_samples, args, stat_writer_fn, args.use_val_set)
+            print_stats(i + 1, server, client_num_samples, stat_writer_fn, use_val_set)
     
     end_time = datetime.now()
 
     print(SECTION_STR.format("Post-Simulation"))
     print(f"Total Simulation time: {end_time - start_time}")
     # Save server model
-    ckpt_path = os.path.join("checkpoints", args.dataset)
+    ckpt_path = os.path.join("checkpoints", dataset)
     if not os.path.exists(ckpt_path):
         os.makedirs(ckpt_path)
-    save_path = server.save_model(os.path.join(ckpt_path, "{}.ckpt".format(args.model)))
+    save_path = server.save_model(os.path.join(ckpt_path, "{}.ckpt".format(model)))
     print(f"Model saved in path: {save_path}")
 
 
@@ -208,26 +214,26 @@ def setup_clients(
     return clients
 
 
-def get_stat_writer_function(ids, groups, num_samples, args):
+def get_stat_writer_function(ids, groups, num_samples, metrics_name: str, metrics_dir: str):
 
     def writer_fn(num_round, metrics, partition):
         metrics_writer.print_metrics(
-            num_round, ids, metrics, groups, num_samples, partition, args.metrics_dir, "{}_{}".format(args.metrics_name, "stat"))
+            num_round, ids, metrics, groups, num_samples, partition, metrics_dir, "{}_{}".format(metrics_name, "stat"))
 
     return writer_fn
 
 
-def get_sys_writer_function(args):
+def get_sys_writer_function(metrics_name: str, metrics_dir: str):
 
     def writer_fn(num_round, ids, metrics, groups, num_samples):
         metrics_writer.print_metrics(
-            num_round, ids, metrics, groups, num_samples, "train", args.metrics_dir, "{}_{}".format(args.metrics_name, "sys"))
+            num_round, ids, metrics, groups, num_samples, "train", metrics_dir, "{}_{}".format(metrics_name, "sys"))
 
     return writer_fn
 
 
 def print_stats(
-    num_round, server, clients, num_samples, args, writer, use_val_set):
+    num_round, server, num_samples, writer, use_val_set):
     
     train_stat_metrics = server.eval_model(set_to_use="train")
     print_metrics(train_stat_metrics, num_samples, prefix="train_")
@@ -258,7 +264,3 @@ def print_metrics(metrics, weights, prefix=""):
                  np.percentile(ordered_metric, 10),
                  np.percentile(ordered_metric, 50),
                  np.percentile(ordered_metric, 90)))
-
-
-if __name__ == "__main__":
-    main()
