68b862cfba347ecf1f4b15ba08f687796553372b
Richard Hu <r.hu@berkeley.edu>
2022-06-25 22:11:10 -0700

    Fix param names in shakespeare/stacked_lstm.py

--- rayleaf/models/shakespeare/stacked_lstm.py
+++ rayleaf/models/shakespeare/stacked_lstm.py
@@ -7,16 +7,16 @@ from rayleaf.utils.language_utils import letter_to_index, word_to_indices
 
 
 class ClientModel(Model):
-    def __init__(self, seed, lr, seq_len, num_classes, n_hidden):
+    def __init__(self, seed, lr, seq_len, num_classes, num_hidden):
         super(ClientModel, self).__init__(seed, lr)
 
         self.seq_len = seq_len
         self.num_classes = num_classes
-        self.n_hidden = n_hidden
+        self.num_hidden = num_hidden
 
         self.embedding = nn.Embedding(self.num_classes, 8)
-        self.lstm = nn.LSTM(input_size=8, hidden_size=self.n_hidden, num_layers=2, batch_first=True)
-        self.fc1 = nn.Linear(self.n_hidden * 2, self.num_classes)
+        self.lstm = nn.LSTM(input_size=8, hidden_size=self.num_hidden, num_layers=2, batch_first=True)
+        self.fc1 = nn.Linear(self.num_hidden * 2, self.num_classes)
 
         self.loss_fn = nn.CrossEntropyLoss()
         self.optimizer = self.optimizer(self.parameters(), lr=self.lr)
@@ -30,7 +30,7 @@ class ClientModel(Model):
     def forward(self, x):
         embedding = self.embedding(x)
         _, (hn, _) = self.lstm(embedding)
-        hn = hn.transpose(0, 1).reshape(-1, 2 * self.n_hidden)
+        hn = hn.transpose(0, 1).reshape(-1, 2 * self.num_hidden)
         logits = self.fc1(hn)
 
         return logits
