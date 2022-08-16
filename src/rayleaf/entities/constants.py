ba3e96f9153c21ceae199c80138892347912905e
Richard Hu <r.hu@berkeley.edu>
2022-08-15 22:49:36 -0700

    Reorganize project

--- src/rayleaf/entities/constants.py
+++
@@ -1,6 +0,0 @@
-CLIENT_ID_KEY = "client_id"
-NUM_SAMPLES_KEY = "num_samples"
-UPDATE_KEY = "update"
-FLOPS_KEY = "flops"
-METRICS_KEY = "metrics"
-ROUND_KEY = "round"
--- src/rayleaf/models/utils.py
+++
@@ -1,7 +0,0 @@
-def number_of_correct(preds, target):
-    return (preds.squeeze() == target).sum().item()
-
-
-def get_predicted_labels(preds):
-    return preds.argmax(dim=-1)
-    
